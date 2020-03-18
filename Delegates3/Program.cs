f5401f5d4f23b647f7343864b1d2b1beeaaf4ba3
Israel Mendoza <xander_irsl@hotmail.com>
2020-03-17 21:30:49 -0600

    Added more examples of Delegates

---
+++ Delegates3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegates3
+{
+    public delegate void SampleDelegate(out int number);
+        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Multicasting            
+            SampleDelegate masterDel = SampleMethod1;
+            masterDel += SampleMethod2;
+            masterDel += SampleMethod3;
+
+            int randomNumber;
+            masterDel(out randomNumber);
+
+            Console.WriteLine($"After invoking a multicasting delegate, the final result of \"randomNumber\" is {randomNumber}");
+
+        }
+
+        static void SampleMethod1(out int number) => number = 1;
+        static void SampleMethod2(out int number) => number = 2;
+        static void SampleMethod3(out int number) => number = 3;
+
+    }
+}
