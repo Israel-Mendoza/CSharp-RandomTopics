13f5f738ff896b88ed52f382497d477bd35358cd
Israel Mendoza <xander_irsl@hotmail.com>
2020-03-18 18:08:09 -0600

    Added project on how to read from text files

---
+++ TextFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFromText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Reading the whole text
+            string text = File.ReadAllText(@"C:\Users\Israel Mendoza\Documents\Israelo.txt");
+            Console.WriteLine("This is the text, read as a single string: ");
+            Console.WriteLine(text);
+            Console.WriteLine();
+
+            // Reading line by line
+            string[] textLines = File.ReadAllLines(@"C:\Users\Israel Mendoza\Documents\Israelo.txt");
+            Console.WriteLine("This is the text again, read line by line: ");
+            foreach (var line in textLines)
+                Console.WriteLine($"\t{line}");
+
+        }
+    }
+}
